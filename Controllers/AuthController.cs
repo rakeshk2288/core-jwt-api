82c16a8fbb2bf0f831be483f3ac8c6c3c1f8d274
Rakesh Kumar <rakeshk2288@gmail.com>
2022-09-10 17:03:22 +0530

    initail commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace core_jwt_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        public record AuthInput(string Username = "admin", string Password = "admin");
+        public record AuthOutput(string Token);
+        public record AuthError(string ErrorMessage);
+
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AuthInput authInput)
+        {
+            try
+            {
+                if (!authInput.Username.Equals("admin") && !authInput.Password.Equals("admin"))
+                    return NotFound("Username/ Password is invalid");
+
+                var token = GenerateToken(authInput);
+                return Ok(new AuthOutput(token));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new AuthError(ex.Message));
+            }
+        }
+
+        private string GenerateToken(AuthInput authInput)
+        {
+            var secreyKey = _configuration["JWT:SecretKey"];
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secreyKey));
+            var signingCred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name,authInput.Username),
+                new Claim("city", "blr")
+            };
+
+            var jwtToken = new JwtSecurityToken(
+                issuer: _configuration["JWT:Issuer"],
+                audience: _configuration["JWT:Audience"],
+                expires: DateTime.UtcNow.AddMinutes(5),
+                claims: claims,
+                signingCredentials: signingCred
+                );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(jwtToken);
+            return token;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, jwt =>
+{
+    jwt.SaveToken = true;
+    jwt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["JWT:SecretKey"])),
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        ValidIssuer = builder.Configuration["JWT:Issuer"]
+    };
+});
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("cityauth", policy =>
+    {
+        policy.RequireClaim("city", "blr");
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
